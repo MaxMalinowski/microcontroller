e51c9eaa2dfc62d22f2c00b0f87d182b6b3caa19
maxmacbook <max.malinowski@outlook.de>
2019-11-19 07:27:31 +0100

    Resumes ex. 6 Timer

--- timer.c
+++ timer.c
@@ -5,39 +5,40 @@
 #include "inttypes.h"
 
 
+volatile uint32_t milliSec = 0;
+volatile uint32_t mainTime = 0;
+volatile uint32_t greenTime = 0;
+volatile uint32_t backgroundTime = 0;
+
+volatile uint8_t greenOn = 0;
+volatile uint8_t backgroundOn = 0;
+volatile uint8_t UserOn = 0;
+
+
 void Timer_Init(void) 
 {
-	//init GPIO
-	
-	
-	
-	
-	
-	RCC -> APB1ENR |= 0x00000020;	//Enable Timer 7
-	TIM7-> PSC |= 0x00000014;			//prescaler to 20
-	TIM7 -> ARR |= 0x00000FA0;		// auto-reload register to 4000
-	
-	
-	TIM7->DIER |=0x00000001;		//ENABLE interrupt
-	TIM7->CR1 |=0x00000001;		//enable counter
-	
-	/*RCC -> AHB1ENR |= 0x00000003;		// Enable Clock for GPIOA and GPIOB
-	GPIOB -> MODER &= 0x00000030;   // Clear reset values
-	GPIOB -> MODER |= 0x00005500;		// GPIOB 4-7 are outputs
-	GPIOB -> OTYPER |= 0x000000F0;	// Set output type to open-drain
-	GPIOA -> PUPDR |= 0x00001540;		// Set inputs to pull-up*/
-	
-	//Timer inits
+	RCC -> APB1ENR |= 0x00000020;	// enable Timer 7
+	TIM7 -> PSC |= 0x00000014;		// set prescaler to 20
+	TIM7 -> ARR |= 0x00000F9F;		// set auto-reload register to 3999
+	TIM7 -> DIER |=0x00000001;		// enable interrupt
+	TIM7 -> CR1 |=0x00000001;		// enable counter
+}
+
+void TIM7_IRQHandler()
+{
+    TIM7 -> SR = 0;
+    milliSec++;
 }
 
 void Timer_Main(void)
 {
-	//Inits
+    Timer_Init();
 
 	while(1)
 	{
+	    // check times and if time bigger than treshhold, take action
 	
-		//Dauerschleife
+
 	}
 	
 }
