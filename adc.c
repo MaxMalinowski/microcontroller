76d37995f645dd296e9615d8277ea065978c49f5
Thomas Egen <the8651@thi.de>
2019-12-11 08:06:26 +0100

    ex.10 initialization

--- adc.c
+++ adc.c
@@ -12,7 +12,7 @@ void adc_Init(void)
     ADC1 -> CR2 |= 0x00000003;              // turn ADC on, set continuous mode
     ADC1 -> CR1 |= 0x01000000;              // set resolution to 10 bit (not necessary)//now it is 10 bit
     ADC1 -> SQR3 |= 0x0000016A;             // First: ch10 (PC=), Second: ch11 (PC11)
-    ADC1 -> SQR1 |= 0x00000000;             // only one conversion //for what??
+    ADC1 -> SQR1 |= 0x00000000;             // only one conversion //for what?? //not 0x100000?? because of two conversions
     ADC1 -> CR2 |= 0x00000020,              // enable interrupt
     NVIC_SetPriority(ADC_IRQn, 4);          // set interrupt priority
     NVIC_EnableIRQ(ADC_IRQn);               // enable interrupt
@@ -23,6 +23,7 @@ void adc_GetPotis(uint16_t* firstPoti, uint16_t* secondPoti)
     ADC1 -> CR2 |= 0x40000000;              // start conversion
     while ((ADC1 -> SR & 0x00000002) != 0x00000002) {}
     *firstPoti = ADC1 -> DR;                 // result of first conversion
+		//ADC1_SR = 0????
     while ((ADC1 -> SR & 0x00000002) != 0x00000002) {}
     *secondPoti = ADC1 -> DR;               // result of second conversion
 }
--- throttle.c
+++ throttle.c
@@ -6,7 +6,16 @@
 
 void throttle_Init(void)
 {
-
+	RCC -> AHB1ENR |=0xF;	//ENABLE PB, PA, PD, PC 
+	GPIOD -> MODER |= 0x1000; //PD 6 to output(PD6 is status flag)
+	GPIOD -> OTYPER |= 0x40;	//PD6 output open drain
+	GPIOD -> PUPDR |= 0x1000; 	//PD6 pull up
+	
+	GPIOC -> MODER |= 0x400000;	//PC11 to output(PC11 is enable)
+	
+	GPIOB -> MODER |= 0x5;		//PB0 and PB1 to output (to control the throttle)
+	
+	
 
 }
 
